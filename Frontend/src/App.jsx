96f1fe673288c37cc667d0a50d596a99c34e1202
Tanisha Ali <tanishaali89@gmail.com>
2025-10-23 23:02:20 +0530

    Frontend setup done with Vite and Tailwind

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold">Tanisha Ali</h1>
+        <p className="mt-4">Welcome to my portfolio website!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
